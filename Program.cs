d99929ebab961131eb2a82f76c70e251197d371f
gokul7080 <90996223+gokul7080@users.noreply.github.com>
2021-10-23 21:52:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {Random randomgenerator = new Random();
+        int roll=0;
+        int attempt=0;
+        while(roll!=6)
+        { 
+            roll = randomgenerator.Next(1,7);
+           Console.WriteLine("you rolled :" + roll);
+           attempt++ ;
+        }
+        if(attempt<10)
+        {
+            Console.WriteLine("you have luck");
+        }
+        if(attempt>10)
+        {
+            Console.WriteLine("you have no luck .too much attempts. get lost");
+        }
+        Console.WriteLine("you rolled six in "+ attempt+ " attempts" );
+            
+        }
+    }
+}
